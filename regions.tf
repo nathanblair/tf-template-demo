96f809c14b4bc0b7a22ddfe16e97baa8ec16ad77
Nathan Blair <me@nathanblair.rocks>
2023-08-06 15:19:05 -0400

    range already defaults to iterator starting at 0...

--- regions.tf
+++ regions.tf
@@ -16,5 +16,5 @@ variable "number_of_availability_zones" {
 locals {
   az_map = ["a", "b", "c", ]
 
-  availability_zones = [for each_zone in range(var.number_of_availability_zones) : "${var.default_region}${local.az_map[each_zone - 1]}"]
+  availability_zones = [for each_zone in range(var.number_of_availability_zones) : "${var.default_region}${local.az_map[each_zone]}"]
 }
